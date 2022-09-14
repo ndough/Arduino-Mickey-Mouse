0bc497c7a76a8141f90e73f8915a995632818e51
yamikor <106121863+yamikor@users.noreply.github.com>
2022-09-14 00:08:07 -0400

    Add files via upload

--- Blink.ino
+++ Blink.ino
@@ -129,11 +129,3 @@ void loop(){
     digitalWrite(6, LOW);
     delay(500);
 }
-
-echo "# Blink-Mouse" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/yamikor/Blink-Mouse.git
-git push -u origin main
